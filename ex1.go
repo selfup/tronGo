65b9e0cdeb9c87bfa7a45b203f4f7c4a158b105e
Regis <boudinot.regis@yahoo.com>
2016-02-25 17:41:26 -0700

    Can append previous positions to Bike slice - solid refactor

--- ex1.go
+++ ex1.go
@@ -4,45 +4,39 @@ import "fmt"
 
 // Bike is the bike object. It will keep track of its positions
 type Bike struct {
-	x, y int32
-}
-
-func addPositions(ar []*Bike, bike *Bike) {
-	ar = append(ar, bike)
-	fmt.Println(ar)
-	fmt.Println(ar[0])
+	x       int32
+	y       int32
+	prevPos []*Bike
 }
 
 func (b *Bike) moveUp() {
-	bb := b.y
-	b.y = bb + 1
+	b.y++
+	b.prevPos = append(b.prevPos, b)
 }
 
 func (b *Bike) moveDown() {
-	bb := b.y
-	b.y = bb - 1
+	b.y--
+	b.prevPos = append(b.prevPos, b)
 }
 
 func (b *Bike) moveRight() {
-	bb := b.x
-	b.x = bb + 1
+	b.x++
+	b.prevPos = append(b.prevPos, b)
 }
 
 func (b *Bike) moveLeft() {
-	bb := b.x
-	b.x = bb - 1
+	b.x--
+	b.prevPos = append(b.prevPos, b)
 }
 
 func main() {
-	var previousPositions []*Bike
 	bike := &Bike{x: 0, y: 0}
 
 	bike.moveUp()
-	addPositions(previousPositions, bike)
 	bike.moveRight()
-	addPositions(previousPositions, bike)
-	bike.moveRight()
-	addPositions(previousPositions, bike)
+	bike.moveLeft()
+	bike.moveDown()
+
 	fmt.Print("Current Bike Position: ")
 	fmt.Println(bike)
 }
